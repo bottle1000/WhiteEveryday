7d073f21253fccb90454b260b999723c4c9511d7
박병천 <genius178896@gmail.com>
2026-05-06 00:36:40 +0900

    style: format error code

--- src/main/java/com/whiteeveryday/global/exception/ErrorCode.java
+++ src/main/java/com/whiteeveryday/global/exception/ErrorCode.java
@@ -27,7 +27,6 @@ public enum ErrorCode {
     private final String message;
     private final int status;
 
-
     ErrorCode(String message, int status) {
         this.message = message;
         this.status = status;
